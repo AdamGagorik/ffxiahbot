2eec8c0e007ee612bd6c98d933bb3ec6a003eddc
Adam Gagorik <adam.gagorik@gmail.com>
2014-12-19 13:46:29 -0600

    Add required module imports to top level __init__.py

--- pydarkstar/__init__.py
+++ pydarkstar/__init__.py
@@ -4,3 +4,24 @@ import pydarkstar.logutils
 import logging
 
 pydarkstar.logutils.setError()
+
+try:
+    import sqlalchemy
+except ImportError as e:
+    logging.exception(e.__class__.__name__)
+    logging.error('pip install sqlalchemy')
+    exit(-1)
+
+try:
+    import pymysql
+except ImportError as e:
+    logging.exception(e.__class__.__name__)
+    logging.error('pip install pymysql')
+    exit(-1)
+
+try:
+    import bs4
+except ImportError as e:
+    logging.exception(e.__class__.__name__)
+    logging.error('pip install beautifulsoup4')
+    exit(-1)
