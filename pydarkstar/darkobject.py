37fc7de2cb33e91bb1faac8737ffd59aed8a992d
Adam Gagorik <adam.gagorik@gmail.com>
2014-12-19 13:42:31 -0600

    Add darkobject base class.

---
+++ pydarkstar/darkobject.py
@@ -0,0 +1,17 @@
+"""
+.. moduleauthor:: Adam Gagorik <adam.gagorik@gmail.com>
+"""
+import pydarkstar.logutils
+
+class DarkObject(pydarkstar.logutils.LoggingObject):
+    """
+    Base class for all objects in pydarkstar.
+    """
+    def __init__(self, *args, **kwargs):
+        super(DarkObject, self).__init__(*args, **kwargs)
+
+    def __repr__(self):
+        return '({}) {}'.format(hex(id(self)), self.__class__.__name__)
+
+if __name__ == '__main__':
+    pass
