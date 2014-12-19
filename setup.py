23ae55a6f2cc3ff3cc9d4dca3601d713014c0a57
Adam Gagorik <adam.gagorik@gmail.com>
2014-12-19 13:23:29 -0600

    Add setup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pydarkstar',
+    version='0.1',
+    packages=['tests', 'pydarkstar'],
+    url='git@github.com:AdamGagorik/pydarkstar.git',
+    license='MIT',
+    author='Adam Gagorik',
+    author_email='adam.gagorik@gmail.com',
+    description='A python module for interacting with a darkstar server.'
+)
