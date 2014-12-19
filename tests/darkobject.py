495c937d39da1902948065a38502f9d582fa2b3b
Adam Gagorik <adam.gagorik@gmail.com>
2014-12-19 13:42:42 -0600

    Add darkobject tests.

---
+++ tests/darkobject.py
@@ -0,0 +1,15 @@
+"""
+.. moduleauthor:: Adam Gagorik <adam.gagorik@gmail.com>
+"""
+import unittest
+import pydarkstar.logutils
+import pydarkstar.darkobject
+
+pydarkstar.logutils.setDebug()
+
+class TestDarkObject(unittest.TestCase):
+    def test_init(self):
+        pydarkstar.darkobject.DarkObject()
+
+if __name__ == '__main__':
+    unittest.main()
