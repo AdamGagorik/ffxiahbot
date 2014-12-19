0c028331171633af8a43c916f41311e8c2d702fa
Adam Gagorik <adam.gagorik@gmail.com>
2014-12-19 13:58:46 -0600

    Add ORM Base.

---
+++ pydarkstar/tables/base.py
@@ -0,0 +1,10 @@
+"""
+.. moduleauthor:: Adam Gagorik <adam.gagorik@gmail.com>
+"""
+import sqlalchemy.ext.declarative
+
+Base = sqlalchemy.ext.declarative.declarative_base()
+metadata = Base.metadata
+
+if __name__ == '__main__':
+    pass
