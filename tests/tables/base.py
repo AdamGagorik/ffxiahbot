881416e43ab64c57a000af6e83131be32d291c58
Adam Gagorik <adam.gagorik@gmail.com>
2014-12-19 14:00:08 -0600

    Add ORM Base tests.

---
+++ tests/tables/base.py
@@ -0,0 +1,15 @@
+"""
+.. moduleauthor:: Adam Gagorik <adam.gagorik@gmail.com>
+"""
+import unittest
+import pydarkstar.logutils
+import pydarkstar.tables.base
+
+pydarkstar.logutils.setDebug()
+
+class TestBase(unittest.TestCase):
+    def test_init(self):
+        pydarkstar.tables.base.Base()
+
+if __name__ == '__main__':
+    unittest.main()
