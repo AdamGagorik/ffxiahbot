7de5d0df0b726c629724d931766ede629a451d18
Adam Gagorik <adam.gagorik@gmail.com>
2014-12-19 13:42:17 -0600

    Add logging tests.

---
+++ tests/logutils.py
@@ -0,0 +1,13 @@
+"""
+.. moduleauthor:: Adam Gagorik <adam.gagorik@gmail.com>
+"""
+import unittest
+import pydarkstar.logutils
+
+pydarkstar.logutils.setDebug()
+
+class TestLogutils(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
