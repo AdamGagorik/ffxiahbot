febf9cb3ed38bfbf15ede89acb598bc9df24fc07
Adam Gagorik <adam.gagorik@gmail.com>
2014-12-21 17:02:49 -0500

    Add itemlist savecsv test.

--- tests/itemlist.py
+++ tests/itemlist.py
@@ -4,6 +4,8 @@
 import unittest
 import pydarkstar.logutils
 import pydarkstar.itemlist
+import tempfile
+import os
 
 pydarkstar.logutils.setDebug()
 
@@ -40,6 +42,17 @@ class TestItemList(unittest.TestCase):
         ilist.add(0)
         ilist.add(1)
         self.assertEqual(len(ilist), 2)
+
+    def test_savecsv(self):
+        ilist = pydarkstar.itemlist.ItemList()
+        ilist.add(0, 'A', True, True, 10, 20, False, False, 30, 40)
+        i, fname = tempfile.mkstemp()
+        ilist.savecsv(fname)
+        with open(fname, 'rb') as handle:
+            line = handle.readline().strip()
+            line = handle.readline().strip()
+            self.assertEqual(line, '0,A,True,True,10,20,False,False,30,40')
+        os.remove(fname)
 
 if __name__ == '__main__':
     unittest.main()
