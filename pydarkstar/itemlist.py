934eede34dfbaeb39d6daa6522f4f949eb306948
Adam Gagorik <adam.gagorik@gmail.com>
2014-12-21 15:05:42 -0500

    Add itemlist add method.

--- pydarkstar/itemlist.py
+++ pydarkstar/itemlist.py
@@ -3,6 +3,7 @@
 """
 import pydarkstar.darkobject
 import pydarkstar.item
+import collections
 
 class ItemList(pydarkstar.darkobject.DarkObject):
     """
@@ -10,6 +11,12 @@ class ItemList(pydarkstar.darkobject.DarkObject):
     """
     def __init__(self, *args, **kwargs):
         super(ItemList, self).__init__(*args, **kwargs)
+        self.items = collections.OrderedDict()
+
+    def add(self, itemid, *args, **kwargs):
+        i = pydarkstar.item.Item(itemid, *args, **kwargs)
+        if i.itemid in self.items:
+            raise KeyError('duplicate item found: %d' % i.itemid)
 
 if __name__ == '__main__':
     pass
