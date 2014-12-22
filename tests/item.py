3e7db2ebab4f2ab71ff54946eeef3b03b76d2475
Adam Gagorik <adam.gagorik@gmail.com>
2014-12-22 13:48:42 -0500

    Add tests for item.

--- tests/item.py
+++ tests/item.py
@@ -13,5 +13,33 @@ class TestItem(unittest.TestCase):
         self.assertEqual(i0.itemid,  0 )
         self.assertEqual(i0.name  , 'A')
 
+    def test_price01(self):
+        pydarkstar.item.Item(0, price01=+1)
+        with self.assertRaises(ValueError):
+            pydarkstar.item.Item(0, price01=+0)
+
+        with self.assertRaises(ValueError):
+            pydarkstar.item.Item(0, price01=-1)
+
+    def test_price12(self):
+        pydarkstar.item.Item(0, price12=+1)
+        with self.assertRaises(ValueError):
+            pydarkstar.item.Item(0, price12=+0)
+
+        with self.assertRaises(ValueError):
+            pydarkstar.item.Item(0, price12=-1)
+
+    def test_stock01(self):
+        pydarkstar.item.Item(0, stock01=+0)
+
+        with self.assertRaises(ValueError):
+            pydarkstar.item.Item(0, stock01=-1)
+
+    def test_stock12(self):
+        pydarkstar.item.Item(0, stock12=+0)
+
+        with self.assertRaises(ValueError):
+            pydarkstar.item.Item(0, stock12=-1)
+
 if __name__ == '__main__':
     unittest.main()
