54d70226a0c19bf3a7ce39287602a699cfce71f0
Adam Gagorik <adam.gagorik@gmail.com>
2014-12-27 10:54:32 -0500

    Add getPrice function.

--- pydarkstar/auction/query.py
+++ pydarkstar/auction/query.py
@@ -60,5 +60,64 @@ def getStock(session, itemid, stack=False, seller=None):
     # return invalid count
     return 99999
 
+def getPrice(session, itemid, stack=False, seller=None, func=min):
+    """
+    Query for the price of an item.
+
+    :param session: session object created from database
+    :param itemid: item number
+    :param stack: consider stacks
+    :param seller: consider seller
+    :param func: processing function
+
+    :type session: py:class:`pydarkstar.database._Session`
+    :type itemid: int
+    :type stack: int
+    :type seller: int
+    :type func: lambda
+    """
+    try:
+        # make sure stack is an int
+        if stack:
+            stack = 1
+        else:
+            stack = 0
+
+        # ignore seller
+        if seller is None:
+            N = func(
+                session.query(AuctionHouse.sale).filter(
+                    AuctionHouse.itemid == itemid,
+                    AuctionHouse.stack  == stack,
+                    AuctionHouse.sale   != 0,
+            ))
+
+        # consider seller
+        else:
+
+            # make sure seller is valid
+            assert isinstance(seller, int)
+            assert seller >= 0
+
+            N = func(
+                session.query(AuctionHouse.sale).filter(
+                    AuctionHouse.itemid == itemid,
+                    AuctionHouse.seller == seller,
+                    AuctionHouse.stack  == stack,
+                    AuctionHouse.sale   != 0,
+            ))
+
+        # return price
+        return N
+
+    except sqlalchemy.exc.SQLAlchemyError:
+        logging.exception('getPrice has failed')
+
+    except AssertionError:
+        logging.exception('getPrice has failed')
+
+    # return invalid price
+    return -1
+
 if __name__ == '__main__':
     pass
