2cd425488795817a41d73e05e844cdb3a4fe73b5
Adam Gagorik <adam.gagorik@gmail.com>
2015-01-04 17:11:36 -0600

    Add AuctionHouse browser.

---
+++ pydarkstar/auction/browser.py
@@ -0,0 +1,107 @@
+"""
+.. moduleauthor:: Adam Gagorik <adam.gagorik@gmail.com>
+"""
+from pydarkstar.tables.auction_house import AuctionHouse
+import pydarkstar.auction.auctionbase
+import pydarkstar.database
+import sqlalchemy
+
+class Browser(pydarkstar.auction.auctionbase.AuctionBase):
+    """
+    Auction House buyer.
+
+    :param db: database object
+    """
+    def __init__(self, db, *args, **kwargs):
+        super(Browser, self).__init__(db, *args, **kwargs)
+
+
+    def count(self):
+        """
+        Get the number of rows.
+        """
+        with self.scopped_session() as session:
+            return session.query(AuctionHouse).count()
+
+    def getStock(self, itemid, stack=False, seller=None):
+        """
+        Get stock of item.
+
+        :param itemid: item number
+        :param stack: consider stacks
+        :param seller: consider seller
+
+        :type itemid: int
+        :type stack: int
+        :type seller: int
+        """
+        with self.capture(fail=self.fail):
+            # validate
+            itemid = AuctionHouse.validate_itemid(itemid)
+            stack  = AuctionHouse.validate_stack(stack)
+
+            # perform query
+            with self.scopped_session() as session:
+
+                # ignore seller
+                if seller is None:
+                    N = session.query(AuctionHouse).filter(
+                        AuctionHouse.itemid == itemid,
+                        AuctionHouse.stack  == stack,
+                        AuctionHouse.sale   == 0,
+                    ).count()
+                    return N
+
+                # consider seller
+                else:
+                    seller = AuctionHouse.validate_seller(seller)
+                    N = session.query(AuctionHouse).filter(
+                        AuctionHouse.itemid == itemid,
+                        AuctionHouse.seller == seller,
+                        AuctionHouse.stack  == stack,
+                        AuctionHouse.sale   == 0,
+                    ).count()
+                    return N
+
+    def getPrice(self, itemid, stack=False, seller=None, func=sqlalchemy.func.min):
+        """
+        Get price of item.
+
+        :param itemid: item number
+        :param stack: consider stacks
+        :param seller: consider seller
+
+        :type itemid: int
+        :type stack: int
+        :type seller: int
+        """
+        with self.capture(fail=self.fail):
+            # validate
+            itemid = AuctionHouse.validate_itemid(itemid)
+            stack  = AuctionHouse.validate_stack(stack)
+
+            # perform query
+            with self.scopped_session() as session:
+
+                # ignore seller
+                if seller is None:
+                    N = session.query(func(AuctionHouse.sale)).filter(
+                        AuctionHouse.itemid == itemid,
+                        AuctionHouse.stack  == stack,
+                        AuctionHouse.sale   != 0,
+                    ).scalar()
+                    return N
+
+                # consider seller
+                else:
+                    seller = AuctionHouse.validate_seller(seller)
+                    N = session.query(func(AuctionHouse.sale)).filter(
+                        AuctionHouse.itemid == itemid,
+                        AuctionHouse.seller == seller,
+                        AuctionHouse.stack  == stack,
+                        AuctionHouse.sale   != 0,
+                    ).scalar()
+                    return N
+
+if __name__ == '__main__':
+    pass
---
+++ tests/auction/browser.py
@@ -0,0 +1,34 @@
+"""
+.. moduleauthor:: Adam Gagorik <adam.gagorik@gmail.com>
+"""
+import unittest
+
+import pydarkstar.logutils
+import pydarkstar.database
+import pydarkstar.auction.browser
+import pydarkstar.rc
+import logging
+
+pydarkstar.logutils.setDebug()
+
+class TestBrowser(unittest.TestCase):
+    def setUp(self):
+        self.db = pydarkstar.database.Database.pymysql(**pydarkstar.rc.sql)
+
+    def test_init(self):
+        pydarkstar.auction.browser.Browser(self.db)
+
+    def test_count(self):
+        browser = pydarkstar.auction.browser.Browser(self.db)
+        browser.count()
+
+    def test_getStock(self):
+        browser = pydarkstar.auction.browser.Browser(self.db)
+        logging.debug('stock = %s', browser.getStock(1, 0))
+
+    def test_getPrice(self):
+        browser = pydarkstar.auction.browser.Browser(self.db)
+        logging.debug('price = %s', browser.getPrice(1, 0))
+
+if __name__ == '__main__':
+    unittest.main()
