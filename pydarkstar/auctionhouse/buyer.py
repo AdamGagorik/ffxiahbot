d73787bc90b13a62a92e4d8687652b166f8f2716
Adam <adam.gagorik@gmail.com>
2015-01-06 20:10:47 -0600

    Minimize buyItem logic.

--- pydarkstar/auctionhouse/buyer.py
+++ pydarkstar/auctionhouse/buyer.py
@@ -3,8 +3,8 @@
 """
 from pydarkstar.tables.auctionhouse import AuctionHouse
 import pydarkstar.auctionhouse.auctionbase
-import pydarkstar.auctionhouse.browser
 import pydarkstar.database
+import pydarkstar.itemlist
 
 class Buyer(pydarkstar.auctionhouse.auctionbase.AuctionBase):
     """
@@ -15,24 +15,20 @@ class Buyer(pydarkstar.auctionhouse.auctionbase.AuctionBase):
     def __init__(self, db, buyer_name='Zissou', *args, **kwargs):
         super(Buyer, self).__init__(db, *args, **kwargs)
         self.buyer_name = str(buyer_name)
-        self.browser = pydarkstar.auctionhouse.browser.Browser(db)
-        self.blacklist = set()
 
     def buyItem(self, row, date, price):
         """
         Buy item for given price.
         """
-        history_price = self.browser.getPrice(row.itemid, row.stack)
+        # validate
+        if row.sale == 0:
+            self.error('item already sold!')
+            return
 
-        # only buy items for a price less than or equal to the history
-        if price <= history_price:
-            row.buyer_name = AuctionHouse.validate_seller(self.buyer_name)
-            row.sell_date  = AuctionHouse.validate_date(date)
-            row.sale       = AuctionHouse.validate_price(price)
-
-        else:
-            self.error('item price too high: %d > %d, seller=%s', price, history_price, row.seller)
-            self.blacklist.add(row.id)
+        row.buyer_name = AuctionHouse.validate_seller(self.buyer_name)
+        row.sell_date  = AuctionHouse.validate_date(date)
+        row.sale       = AuctionHouse.validate_price(price)
+        self.info('%s', row)
 
 if __name__ == '__main__':
     pass
