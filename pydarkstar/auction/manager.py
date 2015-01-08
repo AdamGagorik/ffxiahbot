36a891a6feedd5bf407479a662b7c9a933377eb5
Adam <adam.gagorik@gmail.com>
2015-01-07 20:59:10 -0600

    Add buyItems loop.

--- pydarkstar/auction/manager.py
+++ pydarkstar/auction/manager.py
@@ -6,7 +6,8 @@ import pydarkstar.auction.browser
 import pydarkstar.auction.cleaner
 import pydarkstar.auction.seller
 import pydarkstar.auction.buyer
-import sqlalchemy
+import pydarkstar.timeutils
+import datetime
 
 class Manager(pydarkstar.auction.worker.Worker):
     """
@@ -22,8 +23,73 @@ class Manager(pydarkstar.auction.worker.Worker):
         self.seller  = pydarkstar.auction.seller.Seller(db, **kwargs)
         self.buyer   = pydarkstar.auction.buyer.Buyer(db, **kwargs)
 
+    def addToBlacklist(self, rowid):
+        """
+        Add row to blacklist.
+        """
+        self.debug('blacklisting: row=%d', rowid)
+        self.blacklist.add(rowid)
+
     def buyItems(self, itemdata):
-        pass
+        """
+        The main buy item loop.
+        """
+        with self.scopped_session(fail=self.fail) as session:
+            # find rows that are still up for sale
+            q = session.query(AuctionHouse).filter(
+                    AuctionHouse.seller    != self.seller.seller,
+                    AuctionHouse.sell_date == 0,
+                    AuctionHouse.sale      == 0,
+                )
+            # loop rows
+            for row in q:
+                with self.capture(fail=self.fail):
+                    # skip blacklisted rows
+                    if not row.id in self.blacklist:
+                        # get item data
+                        try:
+                            data = itemdata[row.itemid]
+                        except KeyError:
+                            self.error('item missing from database: %d', row.itemid)
+                            data = None
+
+                        if not data is None:
+                            # buy stacks
+                            if row.stack:
+                                # check permissions
+                                if data.buy12:
+                                    # check price
+                                    if row.price <= data.price12:
+                                        date = pydarkstar.timeutils.timestamp(datetime.datetime.now())
+                                        self.buyer.buyItem(row, date, data.price12)
+                                    else:
+                                        self.debug('price too high! itemid=%d %d <= %d',
+                                            row.itemid, row.price, data.price12)
+                                        self.addToBlacklist(row.id)
+                                else:
+                                    self.debug('not allowed to buy item! itemid=%d', row.itemid)
+                                    self.addToBlacklist(row.id)
+                            # buy singles
+                            else:
+                                # check permissions
+                                if data.buy01:
+                                    # check price
+                                    if row.price <= data.price01:
+                                        date = pydarkstar.timeutils.timestamp(datetime.datetime.now())
+                                        self.buyer.buyItem(row, date, data.price01)
+                                    else:
+                                        self.debug('price too high! itemid=%d %d <= %d',
+                                            row.itemid, row.price, data.price01)
+                                        self.addToBlacklist(row.id)
+                                else:
+                                    self.debug('not allowed to buy item! itemid=%d', row.itemid)
+                                    self.addToBlacklist(row.id)
+                        else:
+                            # item data missing
+                            self.addToBlacklist(row.id)
+                    else:
+                        # row was blacklisted
+                        self.debug('skipping row %d', row.id)
 
     def restockItems(self, itemdata):
         pass
