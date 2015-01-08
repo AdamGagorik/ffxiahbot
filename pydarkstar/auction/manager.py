35cbb00cf738d1965b8e0be98defe9809841a2e3
Adam <adam.gagorik@gmail.com>
2015-01-07 20:14:09 -0600

    Add basic manager structure.

--- pydarkstar/auction/manager.py
+++ pydarkstar/auction/manager.py
@@ -2,6 +2,10 @@ from pydarkstar.tables.auctionhouse import AuctionHouse
 import pydarkstar.auction.worker
 import pydarkstar.database
 import pydarkstar.itemlist
+import pydarkstar.auction.browser
+import pydarkstar.auction.cleaner
+import pydarkstar.auction.seller
+import pydarkstar.auction.buyer
 import sqlalchemy
 
 class Manager(pydarkstar.auction.worker.Worker):
@@ -10,9 +14,13 @@ class Manager(pydarkstar.auction.worker.Worker):
 
     :param db: database object
     """
-    def __init__(self, db, *args, **kwargs):
-        super(Manager, self).__init__(db, *args, **kwargs)
+    def __init__(self, db, **kwargs):
+        super(Manager, self).__init__(db, **kwargs)
         self.blacklist = set()
+        self.browser = pydarkstar.auction.browser.Browser(db, **kwargs)
+        self.cleaner = pydarkstar.auction.cleaner.Cleaner(db, **kwargs)
+        self.seller  = pydarkstar.auction.seller.Seller(db, **kwargs)
+        self.buyer   = pydarkstar.auction.buyer.Buyer(db, **kwargs)
 
     def buyItems(self, itemdata):
         pass
