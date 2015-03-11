4960ab3394e91b75d2578eb69b27c62b312f0b27
Adam Gagorik <adam.gagorik@gmail.com>
2015-03-11 13:15:01 -0500

    Update restock sale date to be in the year 2020.

--- pydarkstar/auction/manager.py
+++ pydarkstar/auction/manager.py
@@ -103,7 +103,7 @@ class Manager(Worker):
 
                 # set history
                 if history_price is None or history_price <= 0:
-                    now = timeutils.timestamp(datetime.datetime(2525, 1, 1))
+                    now = timeutils.timestamp(datetime.datetime(2020, 1, 1))
                     self.seller.setHistory(itemid=data.itemid, stack=False, price=data.price01, date=now, count=1)
 
                 # get stock
@@ -111,7 +111,7 @@ class Manager(Worker):
 
                 # restock
                 while stock < data.stock01:
-                    now = timeutils.timestamp(datetime.datetime(2525, 1, 1))
+                    now = timeutils.timestamp(datetime.datetime(2020, 1, 1))
                     self.seller.sellItem(itemid=data.itemid, stack=False, date=now, price=data.price01, count=1)
                     stock += 1
 
@@ -122,7 +122,7 @@ class Manager(Worker):
 
                 # set history
                 if history_price is None or history_price <= 0:
-                    now = timeutils.timestamp(datetime.datetime(2525, 1, 1))
+                    now = timeutils.timestamp(datetime.datetime(2020, 1, 1))
                     self.seller.setHistory(itemid=data.itemid, stack=True, price=data.price12, date=now, count=1)
 
                 # get stock
@@ -130,7 +130,7 @@ class Manager(Worker):
 
                 # restock
                 while stock < data.stock01:
-                    now = timeutils.timestamp(datetime.datetime(2525, 1, 1))
+                    now = timeutils.timestamp(datetime.datetime(2020, 1, 1))
                     self.seller.sellItem(itemid=data.itemid, stack=True, date=now, price=data.price12, count=1)
                     stock += 1
 
