82d722d3611ee44d2a7da134db38313fd52ee60a
Korrbit <korrbit1@outlook.com>
2019-06-18 10:02:15 -0700

    update to send to dbox  gil for sold item

--- pydarkstar/auction/buyer.py
+++ pydarkstar/auction/buyer.py
@@ -1,4 +1,5 @@
 from ..tables.auctionhouse import AuctionHouse
+from ..tables.deliverybox import DeliveryBox
 from .worker import Worker
 
 
@@ -21,10 +22,31 @@ class Buyer(Worker):
         if not row.sale == 0:
             raise RuntimeError('item already sold!')
 
+        
         row.buyer_name = self.buyer_name
         row.sell_date = AuctionHouse.validate_date(date)
         row.sale = AuctionHouse.validate_price(price)
         self.info('%s', row)
+        self.scopped_session(fail=self.fail) as session:
+            # find rows that are still up for sale
+            q = session.query(DeliveryBox).filter(
+                DeliveryBox.charname == row.seller_name
+            )
+        dbox = DeliveryBox(
+            charid      = row.seller
+            charname    = row.seller_name
+            box         = 1
+            slot        = q.slot +1
+            itemid      = 65535
+            itemsubid   = 0
+            quantity    = row.sale
+            senderid    = 0
+            sender      = self.buyer_name
+            received    = 0
+            sent        = 0
+                    )
+
+        session.add(dbox)
 
 
 if __name__ == '__main__':
