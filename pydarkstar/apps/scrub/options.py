f38065bf9d404cae8f2966c89550e78c19f999fe
Adam Gagorik <adam.gagorik@gmail.com>
2021-06-26 19:56:19 -0400

    Add --server as a command line option

--- pydarkstar/apps/scrub/options.py
+++ pydarkstar/apps/scrub/options.py
@@ -2,8 +2,11 @@
 """
 Create item database.
 """
+import logging
+
 from ...options.basic import BasicOptions
 from ...options.output import OutputOptions
+from ...scrubbing.ffxiah import SERVER_ID
 
 
 class Options(OutputOptions, BasicOptions):
@@ -17,6 +20,7 @@ class Options(OutputOptions, BasicOptions):
         self.stock12 = 5  # default stock for stacks
         self.itemids = []  # a list of item ids
         self.threads = None
+        self.server = 1
         self.urls = []  # a list of category urls
 
         self.add_argument('--urls', type=str, nargs='*', action='append', default=self.urls, metavar='url',
@@ -28,7 +32,10 @@ class Options(OutputOptions, BasicOptions):
         self.add_argument('--stock12', type=int, default=self.stock12, metavar=self.stock12,
                           help='default stock for stacks')
         self.add_argument('--threads', type=int, default=-1, metavar='int',
-                          help='the number of threads to use (default is CPU dependent)')
+                          help='the number of threads (default is CPU dependent)')
+        self.add_argument('--server', type=str, metavar='str | int',
+                          help='the name of the FFXI server to scrub (ex: bahamut)\n'
+                               'this can be a string or a number between 1 and 32')
 
         self.exclude('itemids')
         self.exclude('urls')
@@ -57,6 +64,19 @@ class Options(OutputOptions, BasicOptions):
 
         if not self.itemids:
             self.itemids = None
+
+        try:
+            self.server = int(self.server)
+            if self.server not in SERVER_ID.values():
+                for server, server_id in SERVER_ID.items():
+                    self.error('--server %2d is %s', server_id, server)
+                self.fatal("unknown --server selected: %s", self.server)
+        except ValueError:
+            self.server = str(self.server).lower()
+            if self.server not in SERVER_ID.keys():
+                for server, server_id in SERVER_ID.items():
+                    self.error('--server %2d is %s', server_id, server)
+                self.fatal("unknown --server selected: %s", self.server)
 
 
 if __name__ == '__main__':
