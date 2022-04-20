0edc681f76f9b5026016475a2c90551b0fcfd099
Adam Gagorik <adam.gagorik@gmail.com>
2022-04-19 20:07:26 -0400

    Remove pandoc dependency and use poetry run

--- doc/build.sh
+++ doc/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 conda activate pydarkstar
 ./source/scripts/generate.sh
-sphinx-build -b html -d build/doctrees source build/html
+poetry run sphinx-build -b html -d build/doctrees source build/html
