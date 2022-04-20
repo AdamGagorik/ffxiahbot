641278e1dcf88f2b1f529e39f1d868ee5f82023a
Adam Gagorik <adam.gagorik@gmail.com>
2022-04-19 21:01:20 -0400

    Run version command before Sphinx

--- doc/build.sh
+++ doc/build.sh
@@ -5,4 +5,5 @@ SDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${SDIR}
 
 ./source/scripts/generate.sh
+poetry run poetry-dynamic-versioning
 poetry run sphinx-build -b html -d build/doctrees source build/html
