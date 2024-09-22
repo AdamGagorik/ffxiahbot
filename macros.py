5467c461ffbe68215c6355256843d812448f6cba
Adam Gagorik <adam.gagorik@gmail.com>
2024-09-22 16:28:42 -0400

    Fix rendering of docs

--- macros.py
+++ macros.py
@@ -1,6 +1,25 @@
+import re
 from subprocess import run
 
 from mkdocs_macros.plugin import MacrosPlugin
+
+# fmt: off
+# https://stackoverflow.com/questions/14693701
+ANSI_ESCAPE_8BIT = re.compile(
+    r"""
+    \x1B  # ESC
+    (?:   # 7-bit C1 Fe (except CSI)
+        [@-Z\\-_]
+    |     # or [ for CSI, followed by a control sequence
+        \[
+        [0-?]*  # Parameter bytes
+        [ -/]*  # Intermediate bytes
+        [@-~]   # Final byte
+    )
+    """,
+    re.VERBOSE,
+)
+# fmt: on
 
 
 def define_env(env: MacrosPlugin) -> None:
@@ -8,4 +27,5 @@ def define_env(env: MacrosPlugin) -> None:
     def get_help_message(*args: str):
         command = ["uv", "run", "python", "-m", *args, "--help"]
         message = run(command, capture_output=True).stdout.decode("utf-8")
+        message = ANSI_ESCAPE_8BIT.sub("", message)
         return f"```bash\n➜  {' '.join(command)}\n{message}\n```"
